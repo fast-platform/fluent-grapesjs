7613b2b02563bcf9e7b98df3cc941aa79d83ae0e
tcastrog10 <tcastrog10@gmail.com>
2018-11-15 16:39:02 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import grapesjs from 'grapesjs';
+import { Fluent } from 'fast-fluent';
 
 export default grapesjs.plugins.add('fluent-grapesjs', (editor, opts = {}) => {
   const sm = editor.StorageManager;
+
+  console.log(Fluent.getConfig());
 });
